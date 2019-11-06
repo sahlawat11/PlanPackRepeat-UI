dc618fbddb9b0e4293b5febed2b73e19c4199e37
sai <saikirankondapalli03@gmail.com>
2019-11-05 19:23:40 -0500

    mock retrieve itinerary json

--- src/app/services/carousel-view.service.ts
+++ src/app/services/carousel-view.service.ts
@@ -1,18 +1,27 @@
 import { Itinerary, Destination } from '../models/carouselmodels';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable , of } from 'rxjs';
 
+import {jsonString} from './../../mockjson/retrieveitinerary';
 @Injectable({
   providedIn: 'root'
 })
+
 export class CarouselViewService {
 
   constructor(private authHttp: HttpClient) { }
 
-  getItineraryInfoDashboard(email: string): Observable<any> {
-      return this.authHttp.get('./../../mockjson/retrieveitinerary.json')
-     ///return this.authHttp.get(`http://travelapp-env-1.ey2unjuyh7.us-east-1.elasticbeanstalk.com/itinerary/getItineraryByEmail/saransh%40gmail.com`);
+  getItineraryInfoDashboard(email: string, isJsonString?: boolean): Observable<Array<any>> {
+      // return this.authHttp.get('./../../mockjson/retrieveitinerary.json')
+      if (isJsonString) {
+        console.log("printing json string")
+        console.log(jsonString)
+        return of(jsonString);
+      } else {
+        return this.authHttp.get(`http://travelapp-env-1.ey2unjuyh7.us-east-1.elasticbeanstalk.com/itinerary/getItineraryByEmail/saransh%40gmail.com`);
+      }
+
   }
 
 
